5cc2e6fba2bb0075beebc75f22acf79cab158750
Mustafa Yumurtacı <mustafa.yumurtaci@hotmail.com>
2018-10-29 21:38:44 +0300

    Section 2 Lecture 33

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /*functional component
 const SearchBar = () => {
@@ -7,28 +7,30 @@ const SearchBar = () => {
  }
 */
 class SearchBar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { term: '' }
-    }
-    render() {
-        // return (<input onChange={this.handleInputChange} />)
-        return (<div>
-            <input
-                //TODO:bi alttaki satırı yazmazsak, input'un valuesi değişmez.
-                //Sadece event trigger etmiş oluruz.
-                value={this.state.term}
-                onChange={event => this.setState({ term: event.target.value })} />
-        </div>)
-    }
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+  render() {
+    // return (<input onChange={this.handleInputChange} />)
+    return (
+      <div className="search-bar">
+        <input
+          //TODO:bi alttaki satırı yazmazsak, input'un valuesi değişmez.
+          //Sadece event trigger etmiş oluruz.
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
+  }
 
-    /*
+  /*
      event handle fonksiyonu:
      handleInputChange(event) {
          console.log(event.target.value)
  
      }
  */
-
 }
-export default SearchBar
+export default SearchBar;
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,24 +1,18 @@
 //herhangi bir state e  ihtiyacı olmadığı için
 //functional component yapılacak.
 //yani herhangi bir veri kaydedilmiyor.
-import React from 'react';
-import VideoListItem from './video_list_item'
-const VideoList = (props) => {
-
-    const videoItems = props.videos.map(video => {
-
-        return (
-            <VideoListItem
-                onVideoSelect={props.onVideoSelect}
-                key={video.etag}
-                video={video} />
-        )
-    })
+import React from "react";
+import VideoListItem from "./video_list_item";
+const VideoList = props => {
+  const videoItems = props.videos.map(video => {
     return (
-        <ul className="col-md-4 list-group">
-
-            {videoItems}
-        </ul>
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video}
+      />
     );
-}
-export default VideoList
+  });
+  return <ul className="col-md-4 list-group">{videoItems}</ul>;
+};
+export default VideoList;
