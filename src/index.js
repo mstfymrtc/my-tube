63ee33ae7837717b2c364d2ed97b266bfccd3b18
Mustafa Yumurtacı <mustafa.yumurtaci@hotmail.com>
2018-10-29 12:39:31 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+    return <div>Hey there!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
